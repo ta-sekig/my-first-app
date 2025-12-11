aa73fce35800b253f05e4fe9f86db913e71cde30
関口　達己 <sekiguchitatsuki@sekiguchitooruonorenoMacBook-Air.local>
2025-12-11 20:10:13 +0900

    Update app.py for DB

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+# PostgreSQL接続用アップデート
+import os
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+database_url=os.environ.get('DATABASE_URL')
+
+if database_url:
+    if database_url.startswich('postgres://'):
+        database_url=database_url.replace('postgres://','postgresql://'1)
+    app.config['SQLALCHEMY_DATABASE_URI']=database_url
+
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
